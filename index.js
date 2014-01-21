49592993db3b544e8a0bc8f97ced728e9e278d25
Jesse Yang <jyyjcc@gmail.com>
2014-01-21 13:47:04 +0800

    fix status cache length

--- index.js
+++ index.js
@@ -1,38 +1,8 @@
-var fs = require('fs');
-var path_ = require('path');
-var staticCache = require('koa-static-cache');
+var fs = require('fs')
+var path_ = require('path')
+var staticCache = require('koa-static-cache')
 
-function isDebug() {
-  return !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
-}
-
-function defaults(a, b) {
-  for (var k in b) {
-    if (!a.hasOwnProperty(k)) {
-      a[k] = b[k];
-    }
-  }
-}
-
-function transformRoutes(routes) {
-  var k, v;
-  for (k in routes) {
-    v = routes[k];
-    if (typeof v === 'string') {
-      routes[k] = new RegExp('^' + v.replace(/\:[^\/]+/g, '([^\/]+)') + '$');
-    }
-  }
-  return routes;
-}
-
-function cleanFileSizes(files) {
-  for (var f in files) {
-    delete files[f].length;
-    delete files[f].etag;
-  }
-}
-
-var ONE_DAY = 24 * 60 * 60;
+var ONE_DAY = 24 * 60 * 60
 var DEFAULTS = {
   routeBase: '/',
   stripSlash: false,
@@ -40,48 +10,82 @@ var DEFAULTS = {
   static: {
     maxAge: 60 * ONE_DAY
   }
-};
+}
+
+function isDebug() {
+  return !process.env.NODE_ENV || process.env.NODE_ENV == 'development'
+}
+
+function defaults(a, b) {
+  for (var k in b) {
+    if (!a.hasOwnProperty(k)) {
+      a[k] = b[k]
+    }
+  }
+}
+
+function transformRoutes(routes) {
+  var k, v
+  for (k in routes) {
+    v = routes[k]
+    if (typeof v === 'string') {
+      routes[k] = new RegExp('^' + v.replace(/\:[^\/]+/g, '([^\/]+)') + '$')
+    }
+  }
+  return routes
+}
+
+function cleanFileSizes(files) {
+  for (var f in files) {
+    delete files[f].length
+    delete files[f].etag
+  }
+}
 
 module.exports = function(directory, options) {
-  options = options || {};
+  options = options || {}
 
-  defaults(options, DEFAULTS);
+  defaults(options, DEFAULTS)
 
-  var debug = 'debug' in options ? options.debug : isDebug();
+  var debug = 'debug' in options ? options.debug : isDebug()
 
   if (debug && !options.static.cacheControl) {
-    options.static.cacheControl = 'no-cache';
+    options.static.cacheControl = 'no-cache'
   }
 
-  var routes = options.routes;
-  var files = {};
-  var serve = staticCache(directory, options.static, files);
-  var index = options.index;
-  var routeBase = options.routeBase.replace(/\/$/, '');
-  var stripSlash = options.stripSlash;
+  var routes = options.routes
+  var files = {}
+  var serve = staticCache(directory, options.static, files)
+  var index = options.index
+  var routeBase = options.routeBase.replace(/\/$/, '')
+  var stripSlash = options.stripSlash
 
   // Clean file size of static-cache when debug
   if (debug) {
-    cleanFileSizes(files);
+    cleanFileSizes(files)
   }
 
-  if (index[0] !== '/') index = '/' + index;
+  if (index[0] !== '/') index = '/' + index
   if (routes) {
-    transformRoutes(options.routes);
+    transformRoutes(options.routes)
   }
   if (routeBase) {
-    routeBase = new RegExp('^' + routeBase);
+    routeBase = new RegExp('^' + routeBase)
   }
 
 
   function getHeaders(filekey) {
-    var filename = path_.join(directory, filekey);
+    var filename = path_.join(directory, filekey)
     if (fs.existsSync(filename)) {
-      var stats = fs.statSync(filename);
-      return {
-        cacheControl: 'no-cache',
-        mtime: stats.mtime.toUTCString()
-      }
+      var obj = {}
+      var stats = fs.statSync(filename)
+      obj.path = filename;
+      obj.cacheControl = options.static.cacheControl
+      obj.maxAge = options.static.maxAge || 0
+      obj.mtime = stats.mtime.toUTCString()
+      obj.length = stats.size
+      obj.etag = obj.mtime
+      return obj
     }
   }
 
@@ -89,73 +93,77 @@ module.exports = function(directory, options) {
     if (this.method != 'GET' &&
         this.method != 'HEAD' &&
         this.method != 'OPTIONS') {
-      this.set('Allow', 'GET,HEAD,OPTIONS');
-      this.throw('Method Not Allowed', 405);
+      this.set('Allow', 'GET,HEAD,OPTIONS')
+      this.throw('Method Not Allowed', 405)
     }
 
-    var path, key, file;
+    var path, key, file
 
-    path = this.path;
+    path = this.path
 
     // when tail is slash
     if (path.length > 2 && path.slice(-1) === '/') {
       // consider as no slash internally
-      path = path.slice(0, -1);
+      path = path.slice(0, -1)
       // if neeed strip slash, do redirect
       if (stripSlash) {
-        this.status = 301;
-        this.redirect(path);
-        return;
+        this.status = 301
+        this.redirect(path)
+        return
       }
     }
 
-    key = routeBase ? path.replace(routeBase, '') : path;
+    key = routeBase ? path.replace(routeBase, '') : path
+    if (key === '/') {
+      key = index;
+      this.path = index;
+    }
     if (key) {
       if (!files[key] && debug) {
         // if not cached, but debugging,
         // try figure out whether this file exists now
-        files[key] = getHeaders(key);
+        files[key] = getHeaders(key)
       }
       if (files[key]) {
-        return yield serve;
+        return yield serve
       }
     }
 
-    var matched = false;
+    var matched = false
 
     if (!routes) {
-      matched = true;
+      matched = true
     } else {
       for (var r in routes) {
         // serve index.html for any path under routes
         if (key.match(routes[r])) {
-          matched = true;
-          break;
+          matched = true
+          break
         }
       }
     }
 
     if (!matched) {
-      this.status = 404;
+      this.status = 404
     }
     // run other middlewares
-    yield next;
+    yield next
 
     if (matched) {
-      this.path = index;
-      yield serve;
+      this.path = index
+      yield serve
     }
   }
-};
+}
 
 module.exports.routeCollector = function(routes) {
-  routes = routes || {};
+  routes = routes || {}
   var ret = function(route, handler) {
     if (route[0] != '/') {
-      route = '/' + route; // add head slash
+      route = '/' + route // add head slash
     }
-    routes[route] = route.replace(/[^^]\/$/, ''); // clean tail slash
-  };
-  ret.routes = routes;
-  return ret;
-};
+    routes[route] = route.replace(/[^^]\/$/, '') // clean tail slash
+  }
+  ret.routes = routes
+  return ret
+}
